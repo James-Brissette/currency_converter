a0152ae6544b3c95ecebe19ddb8fbe1165c791d0
James Brissette <brissette@sci.utah.edu>
2020-02-25 21:56:44 -0700

    Documenting Code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 var express = require('express');
 var app = express();
-var personRoute = require('./routes/person');
+var bodyParser = require('body-parser');
 var currencyRoute = require ('./routes/currency');
-var path = require('path');
 
-app.use((req, res, next) => {
-    console.log(`${new Date().toString()} => ${req.originalUrl}`);
-    next();
-});
-
+app.use(bodyParser.json());
 app.use(currencyRoute);
-app.use(personRoute);
 app.use(express.static('public'));
 
 //Handler for 404 - Resource Not Found
 app.use((req, res, next) => {
-    res.status(404).send('We think you are lost');
+    res.status(404).send("You're a long way from home...");
 });
-
-//Handler for Error 500
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-
-    res.sendFile(path.join(__dirname, '../public/500.html'));
-});
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
--- src/routes/currency.js
+++ src/routes/currency.js
@@ -1,27 +1,74 @@
-let express = require('express');
-let router = express.Router();
-let fs = require('fs');
-let xml2js = require('xml2js');
-let parser = xml2js.Parser();
-let Promise = require('promise');
-var readFile = Promise.denodeify(require('fs').readFile);
+var express = require('express');
+var router = express.Router();
+var Promise = require('promise');
+var fs = require('fs');
+var readFile = Promise.denodeify(fs.readFile);
+var xml2js = require('xml2js');
+var parser = xml2js.Parser();
 var parseString = Promise.denodeify(parser.parseString);
+var https = require('https');
 
 
-/* 
-Request Parameters:
-- base_currency
-- base_amount
-- target_currency
-
-Response Parameters:
-- target_currency
-- target_amount 
+/**
+*  GET Route for the '/currency' endpoint
+*  @param  {JSON Object} req Takes in a JSON request body in the format:
+*   {
+*     base_currency:   XXX,
+*     base_amount:   XXXXX,
+*     target_currency: XXX
+*   }
+*
+*  @return {JSON String} The target currency and amount in the format:
+*   {
+*     results: {
+*         target_amount: XXXXX,
+*         target_currency: XXX
+*     }
+*   }
+*
 */
+router.get('/currency', (req, res) => {
+     
+    if (!req.body.base_currency) {
+        return res.send("Please specify a valid base currency");
+    }
+    if (!req.body.base_amount || isNaN(req.body.base_amount)) {
+        return res.send("Please specify a base amount as a number");
+    }
+    if (!req.body.target_currency) {
+        return res.send("Please specify a target currency");
+    }
+    
+    var base_currency = req.body.base_currency;
+    var base_amount = req.body.base_amount;
+    var target_currency = req.body.target_currency;
+    var xml = __dirname + '/../../public/xmlfiles/eurofxref-daily.xml';
 
+    var xml_req = https.get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml", function (result) {
+        let xml = '';
+        // Stream xml data into variable
+        result.on('data', function(input) {
+            xml += input;
+        });
+
+        // Pass resulting xml variable into function convertCurrency and return value as 'result'
+        result.on('end', function() {
+            convertCurrency(xml, base_currency, base_amount, target_currency).then(function (result) {
+                if (result === 'target_currency_error') {
+                    res.send('Please select a valid target currency');
+                } else if (result === 'base_currency_error') {
+                    res.send('Please select a valid base currency');
+                } else {
+                    //Output final response to end user as JSON
+                    res.json({ results: { target_currency: target_currency, target_amount: result } });
+                }
+            });
+        });
+    });
+});
 
 async function convertCurrency(xml, base_currency, base_amount, target_currency) {
-    
+    // Await asynchronous parseString call xml2js.Parser().parseString
     var asyncParseString = async (xml) => {
         try {
             var wait = await parseString(xml);
@@ -31,22 +78,24 @@ async function convertCurrency(xml, base_currency, base_amount, target_currency)
         return wait;
     }
     
+    // Await results of asynchronous parseString call
     var result = async (xml) => {
-        let parsedString = await asyncParseString(xml);
+        var parsedString = await asyncParseString(xml);
         return parsedString;
     }
     
-    out = await readFile(xml, 'utf-8').then(result);
-
+    out = await result(xml);
+    
+    //Extract relevant data from standardized XML template
     rates = out['gesmes:Envelope'].Cube[0].Cube[0].Cube;
     
-    let currencies = rates.map(row => row.$);
+    var currencies = rates.map(row => row.$);
     if (base_currency === 'EUR') {
         var baseRate = 1;
     } else {
         var baseIdx = currencies.map(a => a.currency).indexOf(base_currency);
         if (baseIdx == -1) {
-            res.send("Please enter a valid base currency");
+            return ('base_currency_error');
         }
         var baseRate = currencies[baseIdx].rate * 1
     }
@@ -56,39 +105,15 @@ async function convertCurrency(xml, base_currency, base_amount, target_currency)
     } else {
         var targetIdx = currencies.map(a => a.currency).indexOf(target_currency);
         if (targetIdx == -1) {
-            res.send("Please enter a valid target currency");
+            return ('target_currency_error');
         }
         var targetRate = currencies[targetIdx].rate * 1
     } 
+
 
     var convertedAmount = base_amount / baseRate * targetRate;
     
     return convertedAmount;
 }
-
-router.get('/currency', (req, res) => {
-    //req.query or req.params.{param}
-     
-    if (!req.query.base_currency) {
-        return res.send("Please Specify a Base Currency");
-    }
-    if (!req.query.base_amount) {
-        return res.send("Please Specify a Base Amount");
-    }
-    if (!req.query.target_currency) {
-        return res.send("Please Specify a Target Currency");
-    }
-    
-    var base_currency = req.query.base_currency;
-    var base_amount = req.query.base_amount;
-    var target_currency = req.query.target_currency;
-    var xml = __dirname + '/../../public/xmlfiles/eurofxref-daily.xml';
-
-    convertCurrency(xml, base_currency, base_amount, target_currency).then(function (result) {
-        res.json({ target_currency: target_currency, target_amount: result });
-    });
-    
-
-});
 
 module.exports = router;
